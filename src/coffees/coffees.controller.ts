d544e439ce1db45effc469d9ba02055f6246dcdc
Sanjay <ksanjayk@protonmail.com>
2023-10-04 15:39:51 +0530

    coffee control and service setup

--- src/coffees/coffees.controller.ts
+++ src/coffees/coffees.controller.ts
@@ -1,4 +1,43 @@
-import { Controller } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  HttpCode,
+  HttpStatus,
+  Param,
+  Patch,
+  Post,
+  Query,
+} from '@nestjs/common';
+import { CoffeesService } from './coffees.service';
 
 @Controller('coffees')
-export class CoffeesController {}
+export class CoffeesController {
+  constructor(private readonly coffeesService: CoffeesService) {}
+  @Get()
+  findAll(@Query() paginationQuery) {
+    const { limit, offset } = paginationQuery;
+    return `all coffees Limit ${limit} and offset ${offset}`;
+  }
+
+  @Get(':id')
+  findOne(@Param('id') id: string) {
+    return `${id} coffee`;
+  }
+
+  @Post()
+  create(@Body() body) {
+    return body;
+  }
+
+  @Patch(':id')
+  update(@Param('id') id: string, @Body() body) {
+    return `${id} coffee update`;
+  }
+
+  @Delete(':id')
+  remove(@Param('id') id: string) {
+    return `${id} coffee removed`;
+  }
+}
