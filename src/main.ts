ff6e794c5de7c1a963a35fa49b07e2b2b8c2e398
Sanjay <ksanjayk@protonmail.com>
2023-10-07 15:23:39 +0530

    enabled whitelisting

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,19 @@ import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.useGlobalPipes(new ValidationPipe());
+  // by enabling whitelisting it filters out properties that
+  // that should not be recived by the method handler
+  // by whitelisting any property not listed is
+  // automatically stripped from the resulting object.
+
+  // ForbidNonWhitelisted will stop a request from being processed
+  // if any nonwhitelisted properties are present
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      forbidNonWhitelisted: true,
+    }),
+  );
   await app.listen(3000);
 }
 bootstrap();
