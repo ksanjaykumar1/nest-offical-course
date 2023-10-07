99c79f741694e51971f417330e8d1f762ff0ef3f
Sanjay <ksanjayk@protonmail.com>
2023-10-07 15:10:28 +0530

    PartialType

--- src/coffees/dto/create-coffee.dto/create-coffee.dto.ts
+++ src/coffees/dto/create-coffee.dto/create-coffee.dto.ts
@@ -1,5 +1,11 @@
+import { IsString } from 'class-validator';
+
 export class CreateCoffeeDto {
+  @IsString()
   readonly name: string;
+  @IsString()
   readonly brand: string;
+  // {each: true} indicated the expected values are array of strings
+  @IsString({ each: true })
   readonly flavors: string[];
 }
--- src/coffees/dto/update-coffee.dto/update-coffee.dto.ts
+++ src/coffees/dto/update-coffee.dto/update-coffee.dto.ts
@@ -1,5 +1,8 @@
-export class UpdateCoffeeDto {
-  readonly name?: string;
-  readonly brand?: string;
-  readonly flavors?: string[];
-}
+import { PartialType } from '@nestjs/mapped-types';
+import { CreateCoffeeDto } from '../create-coffee.dto/create-coffee.dto';
+
+// PartialType accpet class and
+// returns the class with all its properties set as optional
+// Partial inherits all the decorators applied to the properperties
+// and adds the additonal property @IsOptional() to all the properties
+export class UpdateCoffeeDto extends PartialType(CreateCoffeeDto) {}
