29810f86c515cf65c8fca1e4e4342bafc9a4fa54
Sanjay <ksanjayk@protonmail.com>
2023-11-21 16:51:59 +0530

    cleared the files

--- src/coffees/dto/update-coffee.dto/update-coffee.dto.ts
+++
@@ -1,8 +0,0 @@
-import { PartialType } from '@nestjs/mapped-types';
-import { CreateCoffeeDto } from '../create-coffee.dto/create-coffee.dto';
-
-// PartialType accpet class and
-// returns the class with all its properties set as optional
-// Partial inherits all the decorators applied to the properperties
-// and adds the additonal property @IsOptional() to all the properties
-export class UpdateCoffeeDto extends PartialType(CreateCoffeeDto) {}
