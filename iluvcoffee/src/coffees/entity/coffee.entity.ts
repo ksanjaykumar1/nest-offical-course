c2286178e576d2f1ae0a07bc594bae841f065fd0
Sanjay <ksanjayk@protonmail.com>
2023-11-26 09:46:17 +0530

    adding metadata for typeorm

--- iluvcoffee/src/coffees/entity/coffee.entity.ts
+++ iluvcoffee/src/coffees/entity/coffee.entity.ts
@@ -1,6 +1,16 @@
+import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
+
+@Entity('Coffee')
 export class Coffee {
+  @PrimaryGeneratedColumn()
   id: number;
+  @Column()
   name: string;
+  @Column()
   brand: string;
+
+  // array is stored as json
+  // by setting nullable to true, we make flavors as optional
+  @Column('json', { nullable: true })
   flavors: string[];
 }
